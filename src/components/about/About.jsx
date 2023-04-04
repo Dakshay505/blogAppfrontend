8ecdd740a2ca89e3371b899b60a1bcd0a7d80947
Dakshay505 <92509352+Dakshay505@users.noreply.github.com>
2023-04-04 17:01:09 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -27,12 +27,12 @@ const About = () => {
         <Box>
             <Banner/>
             <Wrapper>
-                <Typography variant="h3">Code for Interview</Typography>
+                <Typography variant="h3">Dakshay Bishnoi</Typography>
                 <Text variant="h5">I'm a Software Engineer based in India. 
                     I've built websites, desktop applications and corporate software.<br />
                     If you are interested, you can view some of my favorite projects here
                     <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://github.com/kunaltyagi9" color="inherit" target="_blank"><GitHub /></Link>
+                        <Link href="https://github.com/dakshay505" color="inherit" target="_blank"><GitHub /></Link>
                     </Box>
                 </Text>
                 <Text variant="h5">
